4697221d40ec091f06bd1f9c4bb7059fcb11cee5
Thomas O Fredericks <thomas@fredericks.info>
2025-06-12 12:22:49 -0400

    I think I got OSC input working

--- src/PqOSC.h
+++ src/PqOSC.h
@@ -6,7 +6,89 @@
 #include <MicroOscSlip.h>
 #include <MicroOscUdp.h>
 
-template <const size_t MICRO_OSC_IN_SIZE>
+namespace pq
+{
+    // ===============================================================
+    class PqOSCIn; // forward declaration
+    HybridArrayList<PqOSCIn *, PLAQUETTE_MAX_UNITS> _PqoscInList;
+
+    // ===============================================================
+    class PqOSCIn : public Unit
+    {
+
+       // friend class PqOSCSlip; // Grant access
+
+    private:
+        const char *_address;
+        float _value;
+        MicroOsc &_microOsc;
+
+    public:
+        const char *address()
+        {
+            return _address;
+        }
+
+        PqOSCIn(MicroOsc &osc, const char *address) : _microOsc(osc), _address(address)
+        {
+            _PqoscInList.add(this);
+        }
+
+        float put(float f)
+        {
+            _value = f;
+            return get();
+        }
+
+        float get()
+        {
+            return _value;
+        }
+    };
+
+    // ===============================================================
+
+    void _PqOSCMessageCallback(MicroOscMessage &message)
+    {
+        // SHOULD EVENTUALLY CHECK THE SOURCE TO DISTINGUISH MESSAGES FROM DIFFERENT MICROOSC INSTANCES
+        for (size_t i = 0; i != _PqoscInList.size(); i++)
+        {
+            if (message.checkOscAddress(_PqoscInList[i]->address()))
+            {
+                _PqoscInList[i]->put(message.nextAsFloat());
+            }
+        }
+    };
+
+    // ===============================================================
+    /*
+    class PqOSC : public Unit, protected MicroOsc
+    {
+
+        friend class PqOSCIn;  // Grant access
+        friend class PqOSCOut; // Grant access
+
+    protected:
+        MicroOsc &_microOsc;
+
+        PqOSC(MicroOsc &microOsc) : _microOsc(microOsc)
+        {
+        }
+
+        void _add(PqOSCIn *component)
+        {
+            _PqoscInList.add(component);
+        }
+
+        void step() override
+        {
+            _microOsc.onOscMessageReceived(_PqOSCMessageCallback);
+        }
+    };
+*/
+    // ===============================================================
+
+    template <const size_t MICRO_OSC_IN_SIZE>
 class PqOSCSlip : public Unit, public MicroOscSlip<MICRO_OSC_IN_SIZE>
 {
 private:
@@ -34,42 +116,49 @@ public:
    
 };
 
-class PqOscOut : public Unit
-{
-private:
-    MicroOsc &_osc;
-    const char * _address;
-    unsigned long _start;
-    unsigned long _interval;
-    float _value;
+    // ===============================================================
 
-public:
-    PqOscOut(MicroOsc &osc, const char * address, unsigned long interval = 50) : _osc(osc), _address(address), _interval(interval)
+    // ===============================================================
+
+    class PqOSCOut : public Unit
     {
-    }
+    private:
+        MicroOsc &_microOsc;
+        const char *_address;
+        unsigned long _start;
+        unsigned long _interval;
+        float _value;
 
-    float put(float f) {
-        _value = f;
-        return get();
-    }
-
-    float get()
-    {
-        return _value;
-    }
-
-    void begin() {
-        _start = millis();
-    }
-
-    void step()
-    {
-        if ( millis() - _start >= _interval ) {
-            _start = millis();
-
-            _osc.sendFloat(_address, _value);
+    public:
+        PqOSCOut(MicroOsc &osc, const char *address, unsigned long interval = 50) : _microOsc(osc), _address(address), _interval(interval)
+        {
         }
-    }
-};
 
+        float put(float f)
+        {
+            _value = f;
+            return get();
+        }
+
+        float get()
+        {
+            return _value;
+        }
+
+        void begin()
+        {
+            _start = millis();
+        }
+
+        void step()
+        {
+            if (millis() - _start >= _interval)
+            {
+                _start = millis();
+
+                _microOsc.sendFloat(_address, _value);
+            }
+        }
+    };
+}
 #endif
